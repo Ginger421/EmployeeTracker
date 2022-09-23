0e8972f718514e19b54ea7e8cca656398dea7422
Ginger Owen <gingerowen0421@gmail.com>
2022-09-22 21:15:35 -0600

    worked on index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,8 @@
+INSERT INTO departments
+VALUES ("Marketing"), ("Engineering"), ("HR")
 
+INSERT INTO roles(title, salary, department_id)
+VALUES ("Researcher", 60000, 1), ("Software Developer", 90000, 2), ("Recruiter", 80000, 3)
+
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES ("Emily", "Jenkins", 2, null), ("Caroline", "Jenkins", 1, 1), ("Kevin", "Jenkins", 3, 1)
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: '',
+    database: ''
+  },
+  console.log(`Connected to the movies_db database.`)
+);
+
+function menu() {
+    inquirer.createPromptModule([{
+        type: 'list',
+        name: 'menu options',
+        message: 'Please select from the menu options',
+        choices: ["View all Departments", "View roles", "Employees", "Add department", "Add role", "Add employee", "Update employee role", "Quit"],
+    }])
+    .input
+    switch (choices) {
+        case view all:
+            
+            break;
+    
+        default:
+            break;
+    }
+    //end menu function
+    
+}
